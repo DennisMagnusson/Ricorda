3d8c3305e3955ad6ce7f76921f2bf00a3d096979
denmag <dennis.magnusson98@gmail.com>
2017-11-30 08:18:12 -0800

    Revert "Remove unused imports"
    This reverts commit 75f5f05f66445184dea54f00a24d2d58b88fe563.

--- app/src/main/java/com/example/d/studyjournal/RepeatFragment.kt
+++ app/src/main/java/com/example/d/studyjournal/RepeatFragment.kt
@@ -5,12 +5,16 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.CardView
+import android.util.LayoutDirection
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.LinearLayout
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 /**
